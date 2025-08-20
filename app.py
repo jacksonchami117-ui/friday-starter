54a45d6f9457076bcce48e406f4b45903133325b
jacksonchami117-ui <jacksonchami117@hotmail.com>
2025-08-20 05:41:28 +0000

    Hook intro screen into Friday

--- app.py
+++ app.py
@@ -49,6 +49,10 @@ def create_app():
 
     # Routes
     @app.route("/")
+    def intro():
+        return render_template("intro.html")
+
+    @app.route("/home")
     def index():
         return render_template("index.html")
 
